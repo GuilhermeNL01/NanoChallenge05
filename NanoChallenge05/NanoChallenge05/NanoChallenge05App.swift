ca46d325a59471e31150324e611ed63fa52033c0
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2024-02-22 13:20:35 -0300

    Setup

---
+++ NanoChallenge05/NanoChallenge05/NanoChallenge05App.swift
@@ -0,0 +1,17 @@
+//
+//  NanoChallenge05App.swift
+//  NanoChallenge05
+//
+//  Created by Guilherme Nunes Lobo on 22/02/24.
+//
+
+import SwiftUI
+
+@main
+struct NanoChallenge05App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
