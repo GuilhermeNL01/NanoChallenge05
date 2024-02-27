877da5f0b263060ef1385bdda97520d5b1a75962
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2024-02-27 16:19:43 -0300

    Onboarding V2

--- NanoChallenge05/NanoChallenge05/Views/OnboardingView.swift
+++ NanoChallenge05/NanoChallenge05/Views/OnboardingView.swift
@@ -8,9 +8,9 @@
 import SwiftUI
 
 private let onboardingSteps = [
-OnboardingStep(image: "Image", title: "First Screen", description: "eXPLANATION"),
-OnboardingStep(image: "Image", title: "Second Screen", description: "huMAN"),
-OnboardingStep(image: "Image", title: "Third Screen", description: "IA")
+    OnboardingStep(image: "Image", title: "IA Buster", description: "The IA buster is an APP that uses Machine Learning to identify if a text is made by an IA or by a legit person."),
+    OnboardingStep(image: "Image", title: "Human Text", description: " When you submit your text a message will be displayed telling the difference."),
+    OnboardingStep(image: "Image", title: "IA Text", description: "The IA buster can tell if the author of your text is a IA or a human with amazing precision")
 ]
 struct OnboardingView: View {
     @State private var showModal = false
@@ -22,73 +22,66 @@ struct OnboardingView: View {
     }
     
     var body: some View {
-        NavigationView{
-            VStack{
-                HStack{
-                    Spacer()
-                    Button(action: {
-                        self.currentStep = onboardingSteps.count - 1
-                    }){
-                        Text("Skip")
-                            .padding(16)
-                            .foregroundColor(.gray)
-                    }
-                }
-                TabView(selection:$currentStep){
+        NavigationView {
+            VStack {
+                TabView(selection: $currentStep) {
                     ForEach(0..<onboardingSteps.count) { it in
-                        VStack{
+                        VStack {
                             Image(onboardingSteps[it].image)
                                 .resizable()
-                                .frame(width: 350,height: 350)
+                                .frame(width: 350, height: 350)
                             
                             Text(onboardingSteps[it].title)
                                 .font(.title)
                                 .bold()
+                                .foregroundColor(.white)
                             
                             Text(onboardingSteps[it].description)
                                 .multilineTextAlignment(.center)
                                 .padding(.horizontal, 32)
                                 .padding(.top)
-                            
-                            
+                                .foregroundColor(.white)
                         }
                         .tag(it)
+                        .opacity(currentStep == it ? 1 : 0)
+                        .animation(.easeInOut)
                     }
                 }
                 .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-                HStack{
+                
+                HStack {
                     ForEach(0..<onboardingSteps.count) { it in
                         if it == currentStep {
                             Rectangle()
                                 .frame(width: 20, height: 10)
                                 .cornerRadius(10)
                                 .foregroundColor(.accentColor)
+                                .transition(.opacity)
                         } else {
                             Circle()
-                                .frame(width: 10,height: 10)
+                                .frame(width: 10, height: 10)
                                 .foregroundColor(.gray)
+                                .transition(.opacity)
                         }
                     }
                 }
                 .padding(.bottom, 24)
-                NavigationLink(destination: ContentView(), isActive: Binding<Bool>(
-                    get: { self.currentStep >= onboardingSteps.count - 1 },
-                    set: { _ in }
-                )) {
-                    Text(currentStep < onboardingSteps.count - 1 ? "Next" : "Get Started")
-                        .padding(16)
-                        .frame(maxWidth: .infinity)
-                        .background(Color.accentColor)
-                        .cornerRadius(16)
-                        .padding(.horizontal, 16)
+                
+                NavigationLink(destination: ContentView()) {
+                    Text("Get Started")
                         .foregroundColor(.white)
+                        .font(.headline)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(10)
                 }
                 .padding(.bottom, 20)
             }
+            .background(Color.backBlack)
         }
-                .buttonStyle(.plain)
-            }
-        }
+        .buttonStyle(.plain)
+    }
+}
 
 #Preview {
     OnboardingView()
